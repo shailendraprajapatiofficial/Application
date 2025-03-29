3ff54e38e622d3ed8c38189af3f8e1e1eb03450c
Nivan <Nivan@DESKTOP-FKSC388>
2025-03-29 14:10:04 +0530

    feature/controller

--- Application/src/main/java/com/example/controller/Controller.java
+++ Application/src/main/java/com/example/controller/Controller.java
@@ -1,5 +1,17 @@
 package com.example.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class Controller {
+	@GetMapping("/")
+	public String helloworld() {
+		return "Hello World";
+		
+		
+	}
+	
+	
 
 }
