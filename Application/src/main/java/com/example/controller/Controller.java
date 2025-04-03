4c7fcfaa69de5ae9ee45a36802784e3bb8a00558
Nivan <Nivan@DESKTOP-FKSC388>
2025-04-04 00:48:45 +0530

    Add  new add controler

--- Application/src/main/java/com/example/controller/Controller.java
+++ Application/src/main/java/com/example/controller/Controller.java
@@ -1,15 +1,19 @@
 package com.example.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
 	@GetMapping("/")
 	public String helloworld() {
-		return "Hello World";
-		
-		
+		return "Hello World";	
+	}
+	
+	@GetMapping("/add/A/B")
+	public int add(@PathVariable int A , @PathVariable int B) {
+		return (int) (A+B);
 	}
 	
 	
