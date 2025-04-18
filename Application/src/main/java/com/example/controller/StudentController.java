0dcf42557964237483dc39915e0ed4e8953c7323
Nivan <Nivan@DESKTOP-FKSC388>
2025-04-18 21:23:31 +0530

    .

--- Application/src/main/java/com/example/controller/StudentController.java
+++ Application/src/main/java/com/example/controller/StudentController.java
@@ -46,6 +46,7 @@ public class StudentController {
 	@GetMapping("/getStudent")
 	public  ResponseEntity<List<Student>> getStudent() {
 		return new ResponseEntity<List<Student>>(studentService.getallStudent(), HttpStatus.FOUND);
+		
 	}
 
 	@GetMapping("/getStudentInfo")
