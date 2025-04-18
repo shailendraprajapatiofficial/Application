50b59af215cadc2bc434657fb9cffecf6c285fc3
Nivan <Nivan@DESKTOP-FKSC388>
2025-04-18 20:08:51 +0530

    added Response Entity In Student

--- Application/src/main/java/com/example/controller/StudentController.java
+++ Application/src/main/java/com/example/controller/StudentController.java
@@ -3,6 +3,8 @@ package com.example.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -42,8 +44,8 @@ public class StudentController {
 	}
 
 	@GetMapping("/getStudent")
-	public List<Student> getStudent() {
-		return studentService.getallStudent();
+	public  ResponseEntity<List<Student>> getStudent() {
+		return new ResponseEntity<List<Student>>(studentService.getallStudent(), HttpStatus.FOUND);
 	}
 
 	@GetMapping("/getStudentInfo")
